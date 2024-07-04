d7e241d0f878e36164290fe42767bbde593cc7ee
Krishna Borse <125799973+KLI888@users.noreply.github.com>
2024-07-04 22:25:59 +0530

    Update settings.py

--- celery_project/settings.py
+++ celery_project/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = 'django-insecure-@g!qteq!x@y2f+i#ifny&aumec(44+gn2t)98(9_jo5y&dki6i
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://49club.live',
+]
 
 # Application definition
 
