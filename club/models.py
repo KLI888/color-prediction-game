66489f98598e6b8d6afd4298dea4ace47abcfb86
Your Name <krushnaborase40@gmail.com>
2024-07-08 20:48:20 +0530

    dsf

--- club/models.py
+++ club/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from .utils import generate_ref_code
 
+import random
 
 # Create your models here.
 class Profile(models.Model):
